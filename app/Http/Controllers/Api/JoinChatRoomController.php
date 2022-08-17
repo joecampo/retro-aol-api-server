b1529b87e6804cebb19d1ae5300dbf37e4ffa981
Joe Campo <jcampo@gmail.com>
2022-08-17 00:05:45 -0400

    Allow room name to be set for joining and leaving chats

--- app/Http/Controllers/Api/JoinChatRoomController.php
+++ app/Http/Controllers/Api/JoinChatRoomController.php
@@ -6,12 +6,13 @@ use App\Actions\JoinChatRoom;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use App\Services\Command;
+use Illuminate\Http\Request;
 
 class JoinChatRoomController extends Controller
 {
-    public function __invoke(): JsonResponse
+    public function __invoke(Request $request): JsonResponse
     {
-        Command::dispatch(JoinChatRoom::class, 'Welcome');
+        Command::dispatch(JoinChatRoom::class, $request->roomName);
 
         return response()->json('', 200);
     }
--- app/Http/Controllers/Api/LeaveChatRoomController.php
+++ app/Http/Controllers/Api/LeaveChatRoomController.php
@@ -6,12 +6,13 @@ use App\Actions\LeaveChatRoom;
 use App\Http\Controllers\Controller;
 use App\Services\Command;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class LeaveChatRoomController extends Controller
 {
-    public function __invoke(): JsonResponse
+    public function __invoke(Request $request): JsonResponse
     {
-        Command::dispatch(LeaveChatRoom::class, 'Welcome');
+        Command::dispatch(LeaveChatRoom::class, $request->roomName);
 
         return response()->json('', 200);
     }
