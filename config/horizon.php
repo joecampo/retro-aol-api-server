09869620c4c1eec98bf59f21e1ac26a390e77893
Joe Campo <jcampo@gmail.com>
2022-09-25 19:26:00 -0400

    Add more queue workers

--- config/horizon.php
+++ config/horizon.php
@@ -182,7 +182,7 @@ return [
     'environments' => [
         'production' => [
             'supervisor-1' => [
-                'maxProcesses' => 10,
+                'maxProcesses' => 100,
                 'balanceMaxShift' => 1,
                 'balanceCooldown' => 3,
             ],
