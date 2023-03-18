eb6272e28dd58fd444bba92a1edc16b369cc7916
Joe Campo <jcampo@gmail.com>
2023-03-18 19:08:58 -0400

    Add ability to cache message on send with specified id

--- app/Actions/SendChatMessage.php
+++ app/Actions/SendChatMessage.php
@@ -8,13 +8,18 @@ use App\ValueObjects\Packet;
 use Lorisleiva\Actions\Concerns\AsAction;
 use React\Socket\ConnectionInterface;
 use Illuminate\Support\Str;
+use Lorisleiva\Actions\Concerns\WithAttributes;
+use Illuminate\Support\Collection;
 
 class SendChatMessage
 {
     use AsAction;
+    use WithAttributes;
 
-    public function handle(ConnectionInterface $connection, Session $session, string $message): void
+    public function handle(ConnectionInterface $connection, Session $session, string $message, mixed $id = null): void
     {
+        $this->set('session', $session);
+
         if (! $message = Str::ascii($message)) {
             return;
         }
@@ -26,5 +31,31 @@ class SendChatMessage
 
             $connection->write(Packet::make($packet)->prepare());
         });
+
+        $this->addMessageToCache($message, $id);
+    }
+
+    private function addMessageToCache(string $message, mixed $id = null): void
+    {
+        if (! $id) {
+            return;
+        }
+
+        cache()->tags($this->session->id)->forever(
+            'chat_messages',
+            $this->messages()
+                ->push([
+                    'id' => $id,
+                    'datetime' => now()->toString(),
+                    'screenName' => cache()->tags($this->session->id)->get('screen_name'),
+                    'message' => $message,
+                ])
+                ->values()
+        );
+    }
+
+    private function messages(): Collection
+    {
+        return cache()->tags($this->session->id)->get('chat_messages', collect())->take(-100);
     }
 }
